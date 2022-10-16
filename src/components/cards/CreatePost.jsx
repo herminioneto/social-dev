890a192d0b05e70eb94af08a551b9b4700fe3ffa
Herminio Neto <herminio_neto@outlook.com>
2022-10-16 19:04:26 -0300

    Integrado criação de posts ao back end

--- src/components/cards/CreatePost.jsx
+++ src/components/cards/CreatePost.jsx
@@ -1,7 +1,13 @@
 import styled from "styled-components"
+import { useForm } from "react-hook-form"
+import { joiResolver } from "@hookform/resolvers/joi"
+import axios from "axios"
+import { useSWRConfig } from "swr"
+
+import { createPostSchema } from "../../../modules/post/post.schema"
 
 import H4 from "../typography/H4"
-import Textarea from "../inputs/Textarea"
+import ControlledTextarea from "../inputs/ControlledTextarea"
 import Button from "../inputs/Button"
 
 const CreatePostContainer = styled.div`
@@ -39,16 +45,39 @@ const BottomText = styled.div`
 `
 
 function CreatePost ({ username }) {
+  const { mutate } = useSWRConfig()
+
+  const { control, handleSubmit, formState: { isValid }, reset } = useForm({
+    resolver: joiResolver(createPostSchema),
+    mode: 'all'
+  })
+
+  const onSubmit = async (data) => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, data)
+    if (response.status === 201) {
+      reset()
+      mutate(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
+    }
+  }
+
   return (
     <CreatePostContainer>
       <H4><Title>No que você está pensando, @{username}?</Title></H4>
-      <TextContainer>
-        <Textarea placeholder="Digite sua mensagem" rows="5" />
-      </TextContainer>
-      <BottomContainer>
-        <BottomText>A sua mensagem será pública</BottomText>
-        <Button>Publicar</Button>
-      </BottomContainer>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextContainer>
+          <ControlledTextarea 
+            placeholder="Digite sua mensagem" 
+            rows="5" 
+            control={control} 
+            name="text"
+            maxLength="256"
+          />
+        </TextContainer>
+        <BottomContainer>
+          <BottomText>A sua mensagem será pública</BottomText>
+          <Button disabled={!isValid}>Publicar</Button>
+        </BottomContainer>
+      </form>
     </CreatePostContainer>
   )
 }
